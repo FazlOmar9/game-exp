61e70551e9df93e665c6d07bc2f628eb98ab346e
FazlOmar9 <bt22csa026@iiitn.ac.in>
2023-11-10 03:24:43 +0530

    Build Game Cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/apiClient";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface fetchGames {
@@ -29,7 +30,7 @@ const useGames = () => {
         setError(err.message)
       });
 
-    return () => controller.abort();
+    // return () => controller.abort();
   }, []);
 
   return { games, error };
